6ca42c63df1c6ad1e256ca47a1444b863a9a6891
Denis Vorobev <Kurtcobaine192@outlook.com>
2023-08-17 22:48:28 +0300

    add task

---
+++ src/__test__/test.js
@@ -0,0 +1,20 @@
+import Validator from '..';
+
+test('validator test', () => {
+  // eslint-disable-next-line
+    const str = new Validator('a32_-b');
+  const result = str.validateUserName();
+  expect(result).toBe(true);
+});
+
+test('validator test false', () => {
+  const str = new Validator('1a32_-b');
+  const result = str.validateUserName();
+  expect(result).toBe(false);
+});
+
+test('validatorName', () => {
+  const validate = new Validator('den');
+  const { name } = validate;
+  expect(name).toBe('den');
+});
